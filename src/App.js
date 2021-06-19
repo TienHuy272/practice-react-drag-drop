e1198aac8d8c09d858add540bb787306040cc1d8
Nguyen Tien Huy <tienhuy@Nguyens-MacBook-Pro.local>
2021-06-19 17:52:03 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import dataset from './dataset';
+import Column from './Column';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
-function App() {
+const Conatainer = styled.div`
+  display: flex;
+`;
+
+const App = () => {
+  const [data, setData] = useState(dataset);
+
+  //Handle when drag element end
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId, type } = result;
+
+    //if there is no destination
+    if (!destination) {
+      return;
+    }
+
+    //if source and destination is the same
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    //if you're dragging columns will re-order columns
+    if (type === 'column') {
+      const newColumnOrder = Array.from(data.columnOrder);
+      //remove older order of index
+      newColumnOrder.splice(source.index, 1);
+      //add new order of index
+      newColumnOrder.splice(destination.index, 0, draggableId);
+      const newState = {
+        ...data,
+        columnOrder: newColumnOrder,
+      };
+      setData(newState);
+      return;
+    }
+
+    //if you're dragging tasks inside or between columns(droppableId is key of Droppable) ---------------------------------
+    // 'column-1': {
+    //   id: 'column-1',
+    //   title: 'Todo',
+    //   taskIds: ['task-1'],
+    // },
+    const start = data.columns[source.droppableId];
+    const finish = data.columns[destination.droppableId];
+
+    //if dropped inside the same column
+    //   taskIds: ['task-1',..],---------------------------------
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds,
+      };
+
+      const newState = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setData(newState);
+      return;
+    }
+
+    //if dropped in a different columns
+    //remove task from source column ---------------------------------
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStartColumn = {
+      ...start,
+      taskIds: startTaskIds,
+    };
+
+    //add taskid into new taskIds
+    const finishedTaskIds = Array.from(finish.taskIds);
+    finishedTaskIds.splice(destination.index, 0, draggableId);
+    const newFinishColumn = {
+      ...finish,
+      taskIds: finishedTaskIds,
+    };
+
+    const newState = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newStartColumn.id]: newStartColumn,
+        [newFinishColumn.id]: newFinishColumn,
+      },
+    };
+    setData(newState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='all-columns' direction='horizontal' type='column'>
+        {(provided) => (
+          <Conatainer {...provided.droppableProps} ref={provided.innerRef}>
+            {data.columnOrder.map((id, index) => {
+              const column = data.columns[id];
+              const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+
+              return (
+                <Column
+                  key={column.id}
+                  column={column}
+                  tasks={tasks}
+                  index={index}
+                />
+              );
+            })}
+            {provided.placeholder}
+          </Conatainer>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
-}
+};
 
 export default App;
